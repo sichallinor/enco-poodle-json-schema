9d357bcf547f32853b655b935252bb7e14ce9c6e
Simon Challinor <sdchallinor@gmail.com>
2019-10-01 09:56:02 +0100

    Initial release

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+module.exports =  {
+
+	schema_default: require('./schemas/schema_default.json'),   
+
+
+    helloWorld() {
+    	console.log("hello world - module is up and running");
+    },
+
+
+}
---
+++ test/test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var expect = require('chai').expect;
+var poodleJsonSchema = require('../index');
+
+describe('#poodleJsonSchema', function() {
+
+
+    it('should be facilitate access to default schema', () => {  
+
+        console.log(poodleJsonSchema.schema_default);
+        return poodleJsonSchema.schema_default;
+
+    });
+
+})
